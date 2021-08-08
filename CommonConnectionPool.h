e48c8bca14900304414a76a0149564019626364d
Prune <1520817067@qq.com>
2021-08-08 16:11:35 +0800

    Update CommonConnectionPool.h

--- CommonConnectionPool.h
+++ CommonConnectionPool.h
@@ -32,18 +32,18 @@ private:
 	// 扫描超过maxIdleTime时间的空闲连接，进行多余的连接回收
 	void scannerConnectionTask();
 
-	string _ip;				// mysql的ip地址
-	unsigned short _port;	// mysql的端口号
+	string _ip;			// mysql的ip地址
+	unsigned short _port;		// mysql的端口号
 	string _username;		// mysql的用户名
 	string _password;		// mysql的登录密码
 	string _dbname;			// 连接的数据库名称
 	int _initSize;			// 连接池的初始连接量
 	int _maxSize;			// 连接池的最大连接量
 	int _maxIdleTime;		// 连接池最大空闲时间
-	int _connectionTimeout;	// 连接池获取连接的超时时间
+	int _connectionTimeout;		// 连接池获取连接的超时时间
 
 	queue<Connection*> _connectionQue;	// 存储mysql连接的队列
-	mutex _queueMutex;	// 维护连接队列的线程安全互斥锁
-	atomic_int _connectionCnt;	// 记录连接所创建的connection连接的数量
-	condition_variable cv;	// 设置条件变量，用于连接生产线程和连接消费线程的通信
+	mutex _queueMutex;			// 维护连接队列的线程安全互斥锁
+	atomic_int _connectionCnt;		// 记录连接所创建的connection连接的数量
+	condition_variable cv;			// 设置条件变量，用于连接生产线程和连接消费线程的通信
 };
